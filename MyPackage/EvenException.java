bcd1b418bd6b45b5f42db35945f99699624c9879
Akshat1213 <akshat020206@gmail.com>
2024-10-12 01:47:20 +0530

    Removed MyPackage and Odd_Even.java

--- MyPackage/EvenException.java
+++
@@ -1,7 +0,0 @@
-package MyPackage;
-
-public class EvenException extends Exception{
-    EvenException(String m){
-        super(m);
-    }
-}
--- MyPackage/OddException.java
+++
@@ -1,7 +0,0 @@
-package MyPackage;
-
-public class OddException extends Exception{
-    public OddException(String m){
-        super(m);
-    }
-}
